6cc507370d43cd982a2fa8e0b46541c2ef06dade
Mighty Chip <mighty.chip@gmail.com>
2019-04-08 20:34:37 -0400

    Cleaning up the implementation and prettying up my code.

--- vars/slackAnnouncer.groovy
+++ vars/slackAnnouncer.groovy
@@ -1,21 +1,27 @@
+#!/usr/bin/env groovy
+
 def call(buildResult) {
-  switch(buildResultt) {
-    case "SUCCESS":
-      slackSend color: "good",
-                message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} was successful!  Hurrah!!"
-      break
+    //If we have a "null" build result, we can assume that's successful from what I've read... maybe I'm wrong, though...
+    buildResult = buildResult ?: "SUCCESS"
 
-    case "FAILURE":
-      slackSend color: "danger",
-                message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} failed.  Who broke the build?!"
-      break
+    switch(buildResult) {
+        case "SUCCESS":
+            slackSend color: "good",
+                      message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} was successful!  Hurrah!!"
+            break
 
-    case "UNSTABLE":
-      slackSend color: "warning",
-                message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} appears to be unstable.  Someone should investigate."
-      break
-    default:
-      slackSend color: "danger",
-                message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} returned an unexpected result.  See the logs to find out more."
-  }
+        case "FAILURE":
+            slackSend color: "danger",
+                      message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} failed.  Who broke the build?!"
+            break
+
+        case "UNSTABLE":
+            slackSend color: "warning",
+                      message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} appears to be unstable.  Someone should investigate."
+            break
+
+        default:
+            slackSend color: "danger",
+                      message: "${env.JOB_NAME} build ${env.BUILD_NUMBER} returned an unexpected result.  See the logs to find out more."
+    }
 }
